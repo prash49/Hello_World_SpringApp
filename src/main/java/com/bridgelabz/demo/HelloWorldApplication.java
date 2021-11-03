55d82e6375f4e8caaeb4f79e982a32e3ad32c57c
Prashanth <imprash4u@gmail.com>
2021-11-03 12:21:28 +0530

    Added: Helloworld spring app to display Hello message

---
+++ src/main/java/com/bridgelabz/demo/HelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Bridgelabz");
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+}
