b0225bd7b86e7cf799f6d9c4f645472b62224115
Prashanth <imprash4u@gmail.com>
2021-11-03 19:26:37 +0530

    Added: Spring App using REST Controllwer

--- src/main/java/com/bridgelabz/demo/HelloWorldApplication.java
+++ src/main/java/com/bridgelabz/demo/HelloWorldApplication.java
@@ -1,14 +1,28 @@
 package com.bridgelabz.demo;
 
+import com.bridgelabz.demo.component.DemoBean;
+import com.bridgelabz.demo.controller.HelloRestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HelloWorldApplication {
+  //  public static final Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
 
     public static void main(String[] args) {
-        System.out.println("Hello from Bridgelabz");
-        SpringApplication.run(HelloWorldApplication.class, args);
+//        logger.debug("Hello from Bridgelabz");
+//        logger.debug("hello");
+//        logger.info("demo");
+//        logger.error("error");
+//        logger.warn("warning");
+        System.out.println("Hello From Bridgelabz");
+        ApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
+//        DemoBean demoBean = context.getBean(DemoBean.class);
+//        logger.debug("Demo Bean = " + demoBean.toString());
+//        System.out.println(context.getBean(HelloRestController.class));
     }
 
 }
--- src/main/java/com/bridgelabz/demo/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/demo/controller/HelloRestController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
+//    curl localhost:8080/hello -w "\n"
     @RequestMapping(value={"" , "/","/home"})
     public  String sayHello(){
-        return "Hello From BridgeLabz";
+        return "Hello From BridgeLabz Using Rest Controller";
     }
 
 }
