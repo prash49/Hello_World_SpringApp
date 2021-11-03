62e103221993b4bfbbfe308c7e1327db89e98160
Prashanth <imprash4u@gmail.com>
2021-11-03 19:28:40 +0530

    Added:SLF Looger4J

--- src/main/java/com/bridgelabz/demo/HelloWorldApplication.java
+++ src/main/java/com/bridgelabz/demo/HelloWorldApplication.java
@@ -10,19 +10,19 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HelloWorldApplication {
-  //  public static final Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
+    public static final Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
 
     public static void main(String[] args) {
-//        logger.debug("Hello from Bridgelabz");
-//        logger.debug("hello");
-//        logger.info("demo");
-//        logger.error("error");
-//        logger.warn("warning");
+        logger.debug("Hello from Bridgelabz");
+        logger.debug("hello");
+        logger.info("demo");
+        logger.error("error");
+        logger.warn("warning");
         System.out.println("Hello From Bridgelabz");
         ApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
-//        DemoBean demoBean = context.getBean(DemoBean.class);
-//        logger.debug("Demo Bean = " + demoBean.toString());
-//        System.out.println(context.getBean(HelloRestController.class));
+        DemoBean demoBean = context.getBean(DemoBean.class);
+        logger.debug("Demo Bean = " + demoBean.toString());
+        System.out.println(context.getBean(HelloRestController.class));
     }
 
 }
